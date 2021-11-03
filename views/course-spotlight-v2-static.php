729dca57b5957840999f5a3c2ce10707077624f1
nur alam <nuralam@nurs-iMac.local>
2021-11-03 19:51:13 +0600

    quize sidebar on the way

--- views/course-spotlight-v2-static.php
+++ views/course-spotlight-v2-static.php
@@ -1,1 +1,251 @@
-This is for design
+<?php
+
+?>
+<!-- background-image: url(data:image/svg+xml,%3Csvg width='14' height='10' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M.922 5.7a.627.627 0 01-.127-.194.72.72 0 010-.5.6.6 0 01.127-.195l.905-.867a.562.562 0 01.425-.195.592.592 0 01.25.05.665.665 0 01.207.145l2.73 2.734L11.295.82a.658.658 0 01.202-.145.613.613 0 01.484.002.568.568 0 01.191.143l.905.88a.6.6 0 01.127.195.72.72 0 010 .5.628.628 0 01-.127.194l-7.2 7.163a.575.575 0 01-.195.147.655.655 0 01-.5 0 .575.575 0 01-.195-.147L.922 5.7z' fill='%23fff'/%3E%3C/svg%3E); -->
+<div class="tutor-single-lesson-wraper">
+    <div class="tutor-lesson-sidebar">
+        <div class="tutor-sidebar-tabs-wrap">
+            <div class="tutor-lessons-tab">
+                <div class="tutor-lessons-lessons-tab active flex-center">
+                    <span class="ttr-education-filled"></span>
+                    <span class="text-medium-caption color-text-title">Lesson List</span>
+                </div>
+                <div class="tutor-lessons-quiz-tab flex-center">
+                    <span class="ttr-question-filled"></span>
+                    <span class="text-medium-caption color-text-title">
+                        Question & Answer
+                    </span>
+                </div>
+            </div>
+            <div class="tutor-sidebar-tabs-content">
+                <div id="tutor-lesson-sidebar-tab-content" class="tutor-lesson-sidebar-tab-item">
+                    <div class="tutor-topics-in-single-lesson">
+                        <div class="tutor-topics-title d-flex justify-content-between">
+                            <div class="tutor-topics-title-left">
+                                <p class="tutor-topic-subtitle text-regular-caption color-text-subsued">Chapter 1</p>
+                                <h3 class="text-medium-h6 color-text-brand">
+                                    First Topics
+                                </h3>
+                            </div>
+                            <div class="tutor-topics-title-right align-self-end">
+                                <p class="tutor-topic-subtitle text-regular-caption color-text-subsued">3/5</p>
+                            </div>
+                        </div>
+                        <div class="tutor-lessons-under-topic">
+                            <div class="tutor-single-lesson-items active">
+                                <a href="http://tutorv2.local/courses/perfect-diet-meal-plan/lesson/first-lesson/"
+                                    class="tutor-single-lesson-a d-flex justify-content-between" data-lesson-id="">
+                                    <div class="tutor-single-lesson-items-left flex-center">
+                                        <span class="ttr-youtube-brand"></span>
+                                        <span class="lesson_title text-regular-caption color-text-title">First
+                                            Lesson</span>
+                                    </div>
+                                    <div class="tutor-single-lesson-items-right flex-center">
+                                        <span class="text-regular-caption color-text-title">1:10</span>
+                                        <input checked id="" type="checkbox"
+                                            class="tutor-form-check-input tutor-form-check-circle" name="" />
+                                        <input id="" type="checkbox"
+                                            class="tutor-form-check-input tutor-form-check-circle" name="" />
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tutor-topics-in-single-lesson tutor-topic-active">
+                        <div class="tutor-topics-title">
+                            <h3>First Topics</h3>
+                            <button class="tutor-single-lesson-topic-toggle">
+                                <i class="tutor-icon-plus"></i>
+                            </button>
+                        </div>
+                        <div class="tutor-lessons-under-topic">
+                            <div class="tutor-single-lesson-items">
+                                <a href="http://tutorv2.local/courses/perfect-diet-meal-plan/lesson/draft-lesson/"
+                                    class="tutor-single-lesson-a" data-lesson-id="14248">
+                                    <i class="tutor-icon-document"></i>
+                                    <span class="lesson_title">Draft Lesson</span>
+                                    <span class="tutor-lesson-right-icons">
+                                        <i class="tutor-lesson-complete "></i>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="tutor-lesson-sidebar-qa-tab-content" class="tutor-lesson-sidebar-tab-item"
+                    style="display: none;">
+                    <h1>This feature has been disabled by the administrator</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="tutor-single-entry-content" class="tutor-quiz-single-entry-wrap tutor-single-entry-content sidebar-hidden">
+        <div class="tutor-single-page-top-bar  d-flex align-items-center justify-content-between">
+            <div class="tutor-topbar-item tutor-hide-sidebar-bar">
+                <a href="javascript:;" class="tutor-lesson-sidebar-hide-bar"><i class="tutor-icon-angle-left"></i> </a>
+                <a href="http://tutorv2.local/courses/perfect-diet-meal-plan/" class="tutor-topbar-home-btn">
+                    <i class="tutor-icon-home"></i> Go to Course Home </a>
+            </div>
+            <div class="tutor-topbar-item tutor-topbar-content-title-wrap">
+                <i class="tutor-icon-youtube"></i> First Lesson
+            </div>
+
+            <div class="tutor-topbar-item tutor-topbar-mark-to-done">
+                <div class="tutor-single-lesson-segment tutor-lesson-complete-form-wrap">
+                    <form method="post">
+                        <input type="hidden" id="_tutor_nonce" name="_tutor_nonce" value="debecd9243"><input
+                            type="hidden" name="_wp_http_referer" value="/wp-admin/admin-ajax.php">
+                        <input type="hidden" value="14238" name="lesson_id">
+                        <input type="hidden" value="tutor_complete_lesson" name="tutor_action">
+
+                        <button type="submit" class="course-complete-button tutor-btn" name="complete_lesson_btn"
+                            value="complete_lesson"><i class="tutor-icon-mark"></i>Complete Lesson</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="tutor-lesson-content-area">
+
+            <input type="hidden" id="tutor_video_tracking_information"
+                value="{&quot;post_id&quot;:14238,&quot;best_watch_time&quot;:0,&quot;autoload_next_course_content&quot;:false}">
+            <div class="tutor-single-lesson-segment tutor-lesson-video-wrap">
+
+                <div class="plyr plyr--full-ui plyr--video plyr--youtube plyr--fullscreen-enabled plyr--paused plyr--stopped plyr--is-touch plyr__poster-enabled"
+                    id="tutorPlayer">
+                    <div class="plyr__controls"><button class="plyr__controls__item plyr__control" type="button"
+                            data-plyr="play"
+                            aria-label="Play, How To Make a Website With WordPress - 2021 (Beginners Tutorial)"><svg
+                                class="icon--pressed" aria-hidden="true" focusable="false">
+                                <use xlink:href="#plyr-pause"></use>
+                            </svg><svg class="icon--not-pressed" aria-hidden="true" focusable="false">
+                                <use xlink:href="#plyr-play"></use>
+                            </svg><span class="label--pressed plyr__sr-only">Pause</span><span
+                                class="label--not-pressed plyr__sr-only">Play</span></button>
+                        <div class="plyr__controls__item plyr__progress__container">
+                            <div class="plyr__progress"><input data-plyr="seek" type="range" min="0" max="100"
+                                    step="0.01" value="0" autocomplete="off" role="slider" aria-label="Seek"
+                                    aria-valuemin="0" aria-valuemax="100" aria-valuenow="0" id="plyr-seek-2015"
+                                    aria-valuetext="00:00 of 1:45:24"
+                                    style="--value:0%; user-select: none; touch-action: manipulation;"><progress
+                                    class="plyr__progress__buffer" min="0" max="100" value="0" role="progressbar"
+                                    aria-hidden="true">% buffered</progress><span class="plyr__tooltip">00:00</span>
+                            </div>
+                        </div>
+                        <div class="plyr__controls__item plyr__time--current plyr__time" aria-label="Current time">
+                            -1:45:24</div>
+                        <div class="plyr__controls__item plyr__volume"><button type="button" class="plyr__control"
+                                data-plyr="mute"><svg class="icon--pressed" aria-hidden="true" focusable="false">
+                                    <use xlink:href="#plyr-muted"></use>
+                                </svg><svg class="icon--not-pressed" aria-hidden="true" focusable="false">
+                                    <use xlink:href="#plyr-volume"></use>
+                                </svg><span class="label--pressed plyr__sr-only">Unmute</span><span
+                                    class="label--not-pressed plyr__sr-only">Mute</span></button><input
+                                data-plyr="volume" type="range" min="0" max="1" step="0.05" value="1" autocomplete="off"
+                                role="slider" aria-label="Volume" aria-valuemin="0" aria-valuemax="100"
+                                aria-valuenow="100" id="plyr-volume-2015" aria-valuetext="100.0%"
+                                style="--value:100%; user-select: none; touch-action: manipulation;"></div><button
+                            class="plyr__controls__item plyr__control" type="button" data-plyr="captions"><svg
+                                class="icon--pressed" aria-hidden="true" focusable="false">
+                                <use xlink:href="#plyr-captions-on"></use>
+                            </svg><svg class="icon--not-pressed" aria-hidden="true" focusable="false">
+                                <use xlink:href="#plyr-captions-off"></use>
+                            </svg><span class="label--pressed plyr__sr-only">Disable captions</span><span
+                                class="label--not-pressed plyr__sr-only">Enable captions</span></button>
+                        <div class="plyr__controls__item plyr__menu"><button aria-haspopup="true"
+                                aria-controls="plyr-settings-2015" aria-expanded="false" type="button"
+                                class="plyr__control" data-plyr="settings"><svg aria-hidden="true" focusable="false">
+                                    <use xlink:href="#plyr-settings"></use>
+                                </svg><span class="plyr__sr-only">Settings</span></button>
+                            <div class="plyr__menu__container" id="plyr-settings-2015" hidden="">
+                                <div>
+                                    <div id="plyr-settings-2015-home">
+                                        <div role="menu"><button data-plyr="settings" type="button"
+                                                class="plyr__control plyr__control--forward" role="menuitem"
+                                                aria-haspopup="true" hidden=""><span>Captions<span
+                                                        class="plyr__menu__value">Disabled</span></span></button><button
+                                                data-plyr="settings" type="button"
+                                                class="plyr__control plyr__control--forward" role="menuitem"
+                                                aria-haspopup="true" hidden=""><span>Quality<span
+                                                        class="plyr__menu__value">undefined</span></span></button><button
+                                                data-plyr="settings" type="button"
+                                                class="plyr__control plyr__control--forward" role="menuitem"
+                                                aria-haspopup="true"><span>Speed<span
+                                                        class="plyr__menu__value">Normal</span></span></button></div>
+                                    </div>
+                                    <div id="plyr-settings-2015-captions" hidden=""><button type="button"
+                                            class="plyr__control plyr__control--back"><span
+                                                aria-hidden="true">Captions</span><span class="plyr__sr-only">Go back to
+                                                previous menu</span></button>
+                                        <div role="menu"></div>
+                                    </div>
+                                    <div id="plyr-settings-2015-quality" hidden=""><button type="button"
+                                            class="plyr__control plyr__control--back"><span
+                                                aria-hidden="true">Quality</span><span class="plyr__sr-only">Go back to
+                                                previous menu</span></button>
+                                        <div role="menu"></div>
+                                    </div>
+                                    <div id="plyr-settings-2015-speed" hidden=""><button type="button"
+                                            class="plyr__control plyr__control--back"><span
+                                                aria-hidden="true">Speed</span><span class="plyr__sr-only">Go back to
+                                                previous menu</span></button>
+                                        <div role="menu"><button data-plyr="speed" type="button" role="menuitemradio"
+                                                class="plyr__control" aria-checked="false"
+                                                value="0.5"><span>0.5×</span></button><button data-plyr="speed"
+                                                type="button" role="menuitemradio" class="plyr__control"
+                                                aria-checked="false" value="0.75"><span>0.75×</span></button><button
+                                                data-plyr="speed" type="button" role="menuitemradio"
+                                                class="plyr__control" aria-checked="true"
+                                                value="1"><span>Normal</span></button><button data-plyr="speed"
+                                                type="button" role="menuitemradio" class="plyr__control"
+                                                aria-checked="false" value="1.25"><span>1.25×</span></button><button
+                                                data-plyr="speed" type="button" role="menuitemradio"
+                                                class="plyr__control" aria-checked="false"
+                                                value="1.5"><span>1.5×</span></button><button data-plyr="speed"
+                                                type="button" role="menuitemradio" class="plyr__control"
+                                                aria-checked="false" value="1.75"><span>1.75×</span></button><button
+                                                data-plyr="speed" type="button" role="menuitemradio"
+                                                class="plyr__control" aria-checked="false"
+                                                value="2"><span>2×</span></button></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><button class="plyr__controls__item plyr__control" type="button" data-plyr="pip"><svg
+                                aria-hidden="true" focusable="false">
+                                <use xlink:href="#plyr-pip"></use>
+                            </svg><span class="plyr__sr-only">PIP</span></button><button
+                            class="plyr__controls__item plyr__control" type="button" data-plyr="fullscreen"><svg
+                                class="icon--pressed" aria-hidden="true" focusable="false">
+                                <use xlink:href="#plyr-exit-fullscreen"></use>
+                            </svg><svg class="icon--not-pressed" aria-hidden="true" focusable="false">
+                                <use xlink:href="#plyr-enter-fullscreen"></use>
+                            </svg><span class="label--pressed plyr__sr-only">Exit fullscreen</span><span
+                                class="label--not-pressed plyr__sr-only">Enter fullscreen</span></button>
+                    </div>
+                    <div class="plyr__video-wrapper plyr__video-embed" style="padding-bottom: 75%;"><iframe
+                            id="youtube-5950" frameborder="0" allowfullscreen="1"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            title="Player for How To Make a Website With WordPress - 2021 (Beginners Tutorial)"
+                            width="640" height="360"
+                            src="https://www.youtube.com/embed/O79pJ7qXwoE?autoplay=0&amp;controls=0&amp;disablekb=1&amp;playsinline=1&amp;cc_load_policy=0&amp;cc_lang_pref=auto&amp;widget_referrer=http%3A%2F%2Ftutorv2.local%2Fcourses%2Fperfect-diet-meal-plan%2Flesson%2Ffirst-lesson%2F&amp;rel=0&amp;showinfo=0&amp;iv_load_policy=3&amp;modestbranding=1&amp;customControls=true&amp;noCookie=false&amp;enablejsapi=1&amp;origin=http%3A%2F%2Ftutorv2.local&amp;widgetid=3"></iframe>
+                        <div class="plyr__poster"
+                            style="background-image: url(&quot;https://i.ytimg.com/vi/O79pJ7qXwoE/maxresdefault.jpg&quot;);">
+                        </div>
+                    </div>
+                    <button type="button" class="plyr__control plyr__control--overlaid" data-plyr="play"
+                        aria-label="Play, How To Make a Website With WordPress - 2021 (Beginners Tutorial)"><svg
+                            aria-hidden="true" focusable="false">
+                            <use xlink:href="#plyr-play"></use>
+                        </svg><span class="plyr__sr-only">Play</span></button>
+                </div>
+
+            </div>
+            <div class="tutor-attachments-wrap">
+            </div>
+            <div class="tutor-next-previous-pagination-wrap">
+                <a class="tutor-next-link tutor-next-link-14300"
+                    href="http://tutorv2.local/courses/perfect-diet-meal-plan/tutor_quiz/q1/">Next →
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
